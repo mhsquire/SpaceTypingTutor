03fb98ce6786a5081cd71154feb3c5c889b8677f
Matt Squire <matt@mhsquire.com>
2024-02-16 08:20:58 -0500

    Text changes to indicate which letters have been pressed.

--- Enemy.gd
+++ Enemy.gd
@@ -9,11 +9,13 @@ signal destroyed(_on_destroy_enemy)
 
 var orange = Color(Color.ORANGE)
 var gray = Color(Color.GRAY)
+var blue = Color(Color.AQUA)
+var red = Color(Color.RED)
 var damage = 0
 var word = []
 
 
-@onready var prompt = $TextToType
+@onready var prompt = $RichTextLabel
 @onready var prompt_text = prompt.text
 @onready var reinforcement_timer = $ReinforcementTimer
 @onready var words = get_node("../../Wordlist")
@@ -23,13 +25,31 @@ func _ready():
 	reinforcement_timer.timeout.connect(_on_reinforcement_timer_timeout)
 
 
+func get_bbcode_color_tag(color: Color) -> String:
+	return "[color=#" + color.to_html(false) + "]"
+
+
+func get_bbcode_end_color_tag() -> String:
+	return "[/color]"
+
+
+func _set_to_center(string: String):
+	return "[center]" + string + "[/center]"
+
+
+func _indicate_character_position(next_character_index: int):
+	var orange_text = get_bbcode_color_tag(blue) + word.substr(0, next_character_index) + get_bbcode_end_color_tag()
+	var blue_text = get_bbcode_color_tag(orange) + word.substr(next_character_index, -1) + get_bbcode_end_color_tag()
+	prompt.parse_bbcode(_set_to_center(orange_text + blue_text))
+
+
 func make_target(make_visible: bool) -> void:
 	if make_visible:
-		prompt.set("theme_override_colors/font_color", orange)
-		prompt.set("theme_override_font_sizes/font_size", 128)
+		prompt.set("theme_override_colors/default_color", orange)
+		prompt.set("theme_override_font_sizes/normal_font_size", 128)
 	else:
-		prompt.set("theme_override_colors/font_color", gray)
-		prompt.set("theme_override_font_sizes/font_size", 96)
+		prompt.set("theme_override_colors/default_color", gray)
+		prompt.set("theme_override_font_sizes/normal_font_size", 96)
 		damage = 0
 		$ProgressBar.value = 100 - damage
 
@@ -41,7 +61,7 @@ func make_destroyed() -> void:
 
 func make_created() -> void:
 	word = words.choose_word()
-	set_prompt_text(word)
+	_set_prompt_text(_set_to_center(word))
 	set_visible(1)
 	damage = 0
 	$ProgressBar.value = 100 - damage
@@ -53,20 +73,19 @@ func get_prompt_text() -> String:
 	return prompt_text
 
 
-func set_prompt_text(new_text: String) -> void:
+func _set_prompt_text(new_text: String) -> void:
 	prompt.text = new_text
 	print("new text = ", new_text)
 
 
 func take_damage() -> void:
-
 	print("Word length = ", word.length())
 	damage = damage + 1
 	$ProgressBar.value = (word.length() - damage) * 100 / word.length()
 	print("Damaged = ", damage)
+	_indicate_character_position(damage)
 	if damage >= word.length():
 		destroyed.emit()
-	
 
 
 func make_timer_start(time_to_wait: int) -> void:
